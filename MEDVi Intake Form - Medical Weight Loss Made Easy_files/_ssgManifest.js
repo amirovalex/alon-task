c3e3e03bf386ecaacfa8c9708cc15fe5fd9a8c9e
amirovalex <amirovalex98@gmail.com>
2024-11-19 18:44:06 +0200

    first commit

---
+++ MEDVi Intake Form - Medical Weight Loss Made Easy_files/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fevent\u002F[flowPublicIdentifier]","\u002Fp\u002F[portalSubdomain]\u002F[[...slug]]","\u002Fp\u002F[portalSubdomain]\u002Flogin","\u002Ft\u002F[flowPublicIdentifier]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
